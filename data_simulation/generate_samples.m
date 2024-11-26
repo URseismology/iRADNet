3206c85f4c20533422dbf2b3cd1df4cb309011db
Meng Wang <meng.wang@rochester.edu>
2024-11-26 13:08:30 -0500

    fix: random generator

--- data_simulation/generate_samples.m
+++ data_simulation/generate_samples.m
@@ -10,6 +10,8 @@ function values = generate_samples(range, n_samples, min_separation)
     % Output:
     %   values        - n_samples x 2 matrix of generated values
 
+    values = zeros(n_samples, 2);
+
     % Extract range boundaries
     r_min = range(1);
     r_max = range(2);
@@ -19,14 +21,14 @@ function values = generate_samples(range, n_samples, min_separation)
         error('Range is too small for the specified minimum separation.');
     end
 
-    % v1 range and rand value
-    range1 = r_max - r_min - min_separation;
-    value1 = r_min + range1 * rand(n_samples, 1);
+    for i = 1:n_samples
+        % Generate v1 in the range [r_min, r_max - min_separation)
+        v1 = r_min + (r_max - r_min - min_separation) * rand();
 
-    % v2 range and rand value
-    range2 = r_max - value1;
-    value2 = value1 + range2 * rand(n_samples, 1);
+        % Generate v2 in the range (v1 + min_separation, r_max]
+        v2 = v1 + min_separation + (r_max - (v1 + min_separation)) * rand();
 
-    % Combine values into a 2-column matrix
-    values = [value1, value2];
+        % Store the values as a row [v1, v2]
+        values(i, :) = [v1, v2];
+    end
 end
